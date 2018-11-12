492c23771d47e85454a2299f07e693fc41118ee1
Kevin Green <kevin.e.green@hotmail.com>
2018-11-12 05:44:28 -0600

    Node stuff

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 console.log('App is running!'); 
 
 const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js')
 
-fs.appendFile('greetings.txt','Hello World', function (err) {
-	if (err) {
-		console.log('Unable to write to file');
-	}
-});
+var user = os.userInfo();
+
+var res = notes.addNote();
+var res2 = notes.add(1,6);
+console.log(res2);
+
+// fs.appendFile('greetings.txt',`Hello ${user.username}! You are  ${notes.age}`, function (err) {
+// 	if (err) {
+// 		console.log('Unable to write to file');
+// 	}
+// });
---
+++ notes.js
@@ -0,0 +1,10 @@
+console.log('Starting Nodes.js');
+
+module.exports.addNote = () => {
+	console.log('Add note');
+	return 'New Node';
+};
+
+module.exports.add = (num1, num2) => {
+	return num1+num2;
+}
